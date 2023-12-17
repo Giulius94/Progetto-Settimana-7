9c3a15851a44a1d9a446b3a73577ca972032e496
Giulius94 <giuliuscb@protonmail.com>
2023-12-17 14:23:03 +0100

    TUTTO OK

--- js/details.js
+++ js/details.js
@@ -29,10 +29,10 @@ function createCard(x) {
         </div>
         <div class="col-md-8">
             <div class="card-body">
-            <h5 class="card-title"> <span>Modello:</span> ${products.name}</h5>
-            <p class="card-text mt-4"> <span>Brand:</span>${products.brand}</p>
-            <p class="card-text mt-4"> <span>Categoria:</span>${products.description}</p>
-            <p class="card-text mt-4 text-body-secondary"> <span>Prezzo:</span> $${products.price}</p>
+            <h5 class="card-title"> <span>Modello: </span> ${products.name}</h5>
+            <p class="card-text mt-4"> <span>Brand: </span>${products.brand}</p>
+            <p class="card-text mt-4"> <span>Caratteristiche: </span>${products.description}</p>
+            <p class="card-text mt-4 text-body-secondary"> <span>Prezzo: </span> $${products.price}</p>
             <h5 class="mt-5"> Spedizioni 24/48H isole incluse </h5>
             <h6 class="mt-5"> Tutti i nostri prodotti vengono sottoposti ad un controllo qualitativo al fine di garantire la soddisfazione del cliente</h6>
             </div>
--- js/script.js
+++ js/script.js
@@ -1,20 +1,4 @@
-
-/*fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
-headers: {
-"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4NDAxNmMwNTgzNTAwMTg1MjMxODkiLCJpYXQiOjE3MDIzNzk1NDIsImV4cCI6MTcwMzU4OTE0Mn0.d8QkvVHhCkTOnICqhbRveiPBpo-KXpuPo1gn473RwKc"
-}
-})
-  .then((response) => response.json())
-  .then((obj) => {
-    console.log(obj);
-    
-  })
-  .catch((error) => console.log("Error!! " + error)); */
-
-
-  /* NON REFRESHARE LA PAGINA INDEX PER FAVORE ABBI PIETà DI NOI*/
-  
-    document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
         if (window.location.pathname.includes('back-office.html')) {
             addProduct();
             fetchProducts();
@@ -126,7 +110,7 @@ function addProduct() {
 
 
 
- fetch("https://striveschool-api.herokuapp.com/api/product/", {
+/*  fetch("https://striveschool-api.herokuapp.com/api/product/", {
 headers: {
 "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4NDAxNmMwNTgzNTAwMTg1MjMxODkiLCJpYXQiOjE3MDIzNzk1NDIsImV4cCI6MTcwMzU4OTE0Mn0.d8QkvVHhCkTOnICqhbRveiPBpo-KXpuPo1gn473RwKc"
 }
@@ -136,7 +120,7 @@ headers: {
     console.log(obj);
     
   })
-  .catch((error) => console.log("Error!! " + error)); 
+  .catch((error) => console.log("Error!! " + error));  */
 
   function fetchProducts() {
     fetch('https://striveschool-api.herokuapp.com/api/product/', {
@@ -169,7 +153,7 @@ function createProductCard(product) {
     card.innerHTML = `
         <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
         <div class="card-body">
-            <h5 class="card-title">Marca: ${product.name}</h5>
+            <h5 class="card-title">Modello: ${product.name}</h5>
             <p class="card-text">Description: ${product.description}</p>
             <p class="card-text">Price: $${product.price}</p>
             <p class="card-text">Brand: ${product.brand}</p>
@@ -180,21 +164,22 @@ function createProductCard(product) {
                 <button class="btn btn-primary edit-btn">Edit</button>
             <div>
         </div>`;
-
         
-
-    // Aggiungi un gestore di eventi per il click del pulsante di eliminazione
+    
     let deleteButton = card.querySelector('.delete-btn');
     deleteButton.addEventListener('click', () => {
-        // Chiama la funzione per eliminare l'elemento
         deleteProduct(product._id);
     });
 
-    // Aggiungi un gestore di eventi per il click del pulsante di invio
+    
     let sendButton = card.querySelector('.send-btn');
     sendButton.addEventListener('click', () => {
-        // Chiama la funzione per inviare la card
         sendProductToHomepage(product);
+    });
+
+    let editButton = card.querySelector('.edit-btn');
+    editButton.addEventListener('click', () => {
+        enableEdit(product);
     });
 
     return card;
@@ -207,13 +192,13 @@ function createCardIndex(product) {
     card.innerHTML = `
         <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
         <div class="card-body">
-            <h5 class="card-title">Marca: ${product.name}</h5>
-            <p class="card-text">Description: ${product.description}</p>
-            <p class="card-text">Price: $${product.price}</p>
+            <h5 class="card-title">Modello: ${product.name}</h5>
+            <p class="card-text">Caratteristiche: ${product.description}</p>
+            <p class="card-text">Prezzo: $${product.price}</p>
             <p class="card-text">Brand: ${product.brand}</p>
             <span class="d-none">${product._id}</span>
             <div class="d-flex justify-content-around"">
-                <button class="btn btn-success send-btn">Description</button>
+                <button class="btn btn-success send-btn">Details</button>
             </div>
         </div>`;
         
@@ -221,42 +206,9 @@ function createCardIndex(product) {
 }
 
 function sendProductToHomepage(product) {
-    // Costruisci l'URL con i parametri della card, incluso l'URL dell'immagine
     const url = `index.html?name=${encodeURIComponent(product.name)}&description=${encodeURIComponent(product.description)}&price=${encodeURIComponent(product.price)}&brand=${encodeURIComponent(product.brand)}&imageUrl=${encodeURIComponent(product.imageUrl)}`;
-
-    // Naviga alla pagina index con l'URL costruito
     window.location.href = url;
 }
-
-
-/* document.addEventListener('DOMContentLoaded', () => {
-    // Recupera i parametri dell'URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const name = urlParams.get('name');
-    const description = urlParams.get('description');
-    const price = urlParams.get('price');
-    const brand = urlParams.get('brand');
-    const imageUrl = urlParams.get('imageUrl');
-
-    if (name && description && price && brand && imageUrl) {
-        // Utilizza i dati come desiderato
-        console.log('Name:', name);
-        console.log('Description:', description);
-        console.log('Price:', price);
-        console.log('Brand:', brand);
-        console.log('Image URL:', imageUrl);
-
-        // Puoi ora appendere i dati nella pagina come preferisci
-        // Ad esempio, puoi creare una nuova card e aggiungerla a un elemento sulla pagina
-        const cardContainer = document.querySelector('#sentCardContainer');
-        const newCard = createCardIndex({ name, description, price, brand, imageUrl });
-        cardContainer.appendChild(newCard);
-    }
-}); */
-
-
-
-
 
     function deleteProduct(productId) {
         fetch(`https://striveschool-api.herokuapp.com/api/product/${productId}`, {
@@ -267,7 +219,6 @@ function sendProductToHomepage(product) {
         })
         .then(response => {
             if (response.ok) {
-                // Rimuovi la card dal DOM dopo l'eliminazione
                 let deletedCard = document.querySelector(`[data-product-id="${productId}"]`);
                 if (deletedCard) {
                     deletedCard.remove();
@@ -296,9 +247,70 @@ function detailsCard() {
        if (e.target.classList.value === 'btn btn-success send-btn') {
         let idCards = e.target.parentNode.parentNode.childNodes[9].innerText;
          let Url = 'dettaglio.html?id=' + idCards;
-         console.log(Url);
          window.open(Url);
        }
     })
+} 
+
+function enableEdit(product) {
+    let form = document.createElement('form');
+    form.setAttribute('id', 'editForm');
+    form.innerHTML = `
+        <div class="mt-2">
+            <label for="text1" class="form-label">Name</label>
+            <input type="text" class="form-control" id="text1" value="${product.name}">
+        </div>
+        <div class="">
+            <label for="text2" class="form-label">Description</label>
+            <input type="text" class="form-control" id="text2" value="${product.description}">
+        </div>
+        <div class="">
+            <label for="text3" class="form-label">Brand</label>
+            <input type="text" class="form-control" id="text3" value="${product.brand}">
+        </div>
+        <div class="">
+            <label for="text4" class="form-label">Image</label>
+            <input type="text" class="form-control" id="text4" value="${product.imageUrl}">
+        </div>
+        <div class="">
+            <label for="text5" class="form-label">Price</label>
+            <input type="text" class="form-control" id="text5" value="${product.price}">
+        </div>
+        <button type="button" id='update' class="btn btn-primary mt-3">Update</button>`;
+
+    let formInput = document.querySelector('#formInput');
+    formInput.innerHTML = '';
+    formInput.appendChild(form);
+
+    
+    let updateButton = form.querySelector('#update');
+    updateButton.addEventListener('click', () => {
+        updateProduct(product._id, {
+            name: document.querySelector('#text1').value,
+            description: document.querySelector('#text2').value,
+            brand: document.querySelector('#text3').value,
+            imageUrl: document.querySelector('#text4').value,
+            price: document.querySelector('#text5').value,
+        });
+    });
 }
 
+function updateProduct(productId, updatedData) {
+    fetch(`https://striveschool-api.herokuapp.com/api/product/${productId}`, {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4NDAxNmMwNTgzNTAwMTg1MjMxODkiLCJpYXQiOjE3MDIzNzk1NDIsImV4cCI6MTcwMzU4OTE0Mn0.d8QkvVHhCkTOnICqhbRveiPBpo-KXpuPo1gn473RwKc"
+        },
+        body: JSON.stringify(updatedData),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Product updated:', data);
+        fetchProducts(); 
+        clearForm();
+    })
+    .catch(error => {
+        console.error('Error updating product:', error);
+    });
+}
